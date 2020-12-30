c233d3cba9ec52415f17ce9da08a7cac5b484181
petebdeblois <petetebdeblois@gmail.com>
2020-12-29 19:44:16 -0500

    update app.test

--- src/app.test.js
+++ src/app.test.js
@@ -2,14 +2,15 @@ const supertest = require('supertest');
 
 const app = require('./app');
 const project = require('./constants/project');
-
+// https://jestjs.io/docs/en/asynchronous
 describe('GET /', () => {
   it('should respond with a message', async () => {
     const response = await supertest(app)
       .get('/')
+      // This expectations are builtin supertest
       .expect('Content-Type', /json/)
       .expect(200);
-
+// this expectations is form JEST
     expect(response.body.message).toEqual(project.message);
   });
 });
