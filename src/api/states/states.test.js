f04ab17990aa2ecb8ab3eac9057e24ebe4fa960b
petebdeblois <petetebdeblois@gmail.com>
2020-12-29 23:04:27 -0500

    update app.test

--- src/api/states/states.test.js
+++ src/api/states/states.test.js
@@ -18,11 +18,11 @@ describe('GET /api/v1/states', () => {
 
   it('should respond with an individual state', async () => {
     const response = await supertest(app)
-      .get('/api/v1/states/52')
+      .get('/api/v1/states/1')
       .expect('Content-Type', /json/)
       .expect(200);
 
-    expect(response.body.id).toBe(52);
+    expect(response.body.id).toBe(1);
   });
 
   it('should respond with a 404 for a not found state', async () => {
